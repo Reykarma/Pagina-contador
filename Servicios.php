6852f9f84d375979e92f4d0f70b7340454a06b6e
Unknown <armando567@hotmail.com>
2017-11-28 19:28:06 -0600

    Servicios
    Se agrego la plantilla de Servicios, y se diseño con imagenes para que sea mas agradable a la vista.

--- Servicios.php
+++ Servicios.php
@@ -9,8 +9,40 @@
     <div class="Cuerpo">
       <?php include("plantillas/Menu.php") ?>
       <article>
+        <div class="Ubicacion">
+          <h2>Servicios</h2>
+        </div>
         <div class="servicios">
-
+          <div class="servicio">
+            <img src="imagenes/auditoria.png" alt="Servicio-1">
+            <h3>Servicio1</h3>
+            <p>s</p>
+          </div>
+          <div class="servicio">
+            <img src="imagenes/auditoria.png" alt="Servicio-2">
+              <h3>Servicio1</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing</p>
+          </div>
+          <div class="servicio">
+            <img src="imagenes/auditoria.png" alt="Servicio-3">
+              <h3>Servicio1</h3>
+              <p>s</p>
+          </div>
+          <div class="servicio">
+            <img src="imagenes/auditoria.png" alt="Servicio-4">
+            <h3>Servicio1</h3>
+            <p>s</p>
+          </div>
+          <div class="servicio">
+            <img src="imagenes/auditoria.png" alt="Servicio-5">
+            <h3>Servicio1</h3>
+            <p>s</p>
+          </div>
+          <div class="servicio">
+            <img src="imagenes/auditoria.png" alt="Servicio-6">
+            <h3>Servicio1</h3>
+            <p>s</p>
+          </div>
         </div>
       </article>
       <?php include("plantillas/Pie.php") ?>
